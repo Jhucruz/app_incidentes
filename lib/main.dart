91ea79bde48e405878434bdc3769931ec58fe56b
jhunior cruz <jhunior cruz@gmail.com>
2025-03-02 20:33:44 -0500

    Feat:Actualizacion 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:app_incidentes/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MainApp());
 }
 
@@ -12,7 +17,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Text('Hola JHUNIOR'),
         ),
       ),
     );
